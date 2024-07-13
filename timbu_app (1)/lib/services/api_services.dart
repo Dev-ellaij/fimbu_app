01cf8b43f00c6e6250b608d0306d948de219978b
ella <111292475+Dev-ellaij@users.noreply.github.com>
2024-07-13 22:59:43 +0100

    Update api_services.dart
    sevices

--- timbu_app (1)/lib/services/api_services.dart
+++ timbu_app (1)/lib/services/api_services.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:fimbu_mobile/model/product_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,14 +12,33 @@ class FimbuServices {
       {required this.api_key,
       required this.app_id,
       required this.organization_id});
+
   Future<Product> getProduct() async {
     final response = await http.get(Uri.parse(
         '$BASE_URL?organization_id=$organization_id&Appid=$app_id&Apikey=$api_key'));
-    //print("response: ${response.body}");
     if (response.statusCode == 200) {
       return Product.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to get retail products');
     }
+  }
+}
+
+void main() async {
+  String apiKey = 'dcb2b39abd57451d96fd4bf0c2ee9fab20240713235028800994';
+  String appId = '7NQZGUWZW0V0CXF';
+  String organizationId = '130aabfbdb984194a0d89870742c05dc';
+
+  FimbuServices fimbuServices = FimbuServices(
+    api_key: apiKey,
+    app_id: appId,
+    organization_id: organizationId,
+  );
+
+  try {
+    Product product = await fimbuServices.getProduct();
+    print('Product: ${product.name}'); // Assuming Product has a field `name`
+  } catch (e) {
+    print(e);
   }
 }
