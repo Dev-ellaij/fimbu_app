d531afd895a92d5f6b6ef180f017cc84710cb31f
ella <111292475+Dev-ellaij@users.noreply.github.com>
2024-07-23 16:38:27 +0100

    Update api_services.dart
    commit

--- timbu_app (1)/lib/services/api_services.dart
+++ timbu_app (1)/lib/services/api_services.dart
@@ -1,44 +1,26 @@
 import 'dart:convert';
-import 'package:fimbu_mobile/model/product_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:tasktask/model/productmodel.dart';
 
-class FimbuServices {
-  static const BASE_URL = 'https://api.timbu.cloud/products';
-  final String api_key;
-  final String app_id;
-  final String organization_id;
+class ProductService {
+  final String BASE_URL;
+  final String apiKey;
+  final String appId;
+  final String organizationId;
 
-  FimbuServices(
-      {required this.api_key,
-      required this.app_id,
-      required this.organization_id});
-
+  ProductService(
+      {required this.BASE_URL,
+      required this.apiKey,
+      required this.appId,
+      required this.organizationId});
   Future<Product> getProduct() async {
     final response = await http.get(Uri.parse(
-        '$BASE_URL?organization_id=$organization_id&Appid=$app_id&Apikey=$api_key'));
+        '$BASE_URL?organization_id=$organizationId&Appid=$appId&Apikey=$apiKey'));
+    //print('response: ${response.body}');
     if (response.statusCode == 200) {
       return Product.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to get retail products');
+      throw Exception('Failed to get product items...');
     }
-  }
-}
-
-void main() async {
-  String apiKey = 'dcb2b39abd57451d96fd4bf0c2ee9fab20240713235028800994';
-  String appId = '7NQZGUWZW0V0CXF';
-  String organizationId = '130aabfbdb984194a0d89870742c05dc';
-
-  FimbuServices fimbuServices = FimbuServices(
-    api_key: apiKey,
-    app_id: appId,
-    organization_id: organizationId,
-  );
-
-  try {
-    Product product = await fimbuServices.getProduct();
-    print('Product: ${product.name}'); 
-  } catch (e) {
-    print(e);
   }
 }
